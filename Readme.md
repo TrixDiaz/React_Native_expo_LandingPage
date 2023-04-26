882ce2ca397f457899e172755238f2e6e1c652a4
TrixDiaz <john.darlucio022@gmail.com>
2023-04-26 13:00:45 +0800

    add v1

--- Readme.md
+++ Readme.md
@@ -7,5 +7,6 @@
 *npm install @react-navigation/native-stack <br>
 *npm install axios react-native-keychain <br>
 *npm install react-native-reanimated react-native-gesture-handler react-native-screens react-native-safe-area-context @react-native-community/masked-view <br>
+npm install react-native-linear-gradient <br>
 *run Device Emulator <br>
 *npx expo start 
--- screens/Home.js
+++ screens/Home.js
@@ -1,22 +1,50 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import React from "react";
 import { useNavigation } from "@react-navigation/native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { ScrollView } from "react-native-gesture-handler";
 
 
 const Home = () => {
-    const navigation = useNavigation();
+  const navigation = useNavigation();
   return (
     <SafeAreaView>
-      <View>
-       
-      </View>
+      <ScrollView>
+        <View style={styles.home}>
+          <TouchableOpacity
+            style={{
+              width: 200,
+              marginHorizontal: 20,
+              marginTop: 50,
+              paddingHorizontal: 20,
+              borderRadius: 20,
+              backgroundColor: "#F9F0FF",
+            }}
+          >
+            <Text style={{ color: "#722ED1" }}>
+              v1.0 release.
+              <Text
+                style={{
+                  textDecorationLine: "underline",
+                }}
+              >
+                Learn more
+              </Text>
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
- 
+  home: {
+    height: 500,
+    width: "100%",
+    borderRadius: 50,
+    backgroundColor: "linear-gradient(to bottom, #00c6fb, #005bea)",
+  },
 });
 
 export default Home;
